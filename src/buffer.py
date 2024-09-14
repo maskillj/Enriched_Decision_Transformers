e8abe8bf716460af7550308887fb173d5e9b6b16
James Maskill <89933770+maskillj@users.noreply.github.com>
2024-09-14 12:36:01 -0400

    Delete src/buffer.py

--- src/buffer.py
+++
@@ -1,7 +0,0 @@
-#from offlinerllib.buffer.base import Buffer
-from abc import ABC, abstractmethod
-
-class Buffer(ABC):
-    @abstractmethod
-    def random_batch(self, batch_size):
-        raise NotImplementedError
