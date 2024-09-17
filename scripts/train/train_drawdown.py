47e3b2dc15265e08d314c622cfb79ca8328baf9b
James Maskill <maskillj@gmail.com>
2024-09-17 17:37:30 -0400

    More work

---
+++ scripts/train/train_drawdown.py
@@ -0,0 +1,95 @@
+if __name__ == '__main__': 
+    import sys
+    import os
+    sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
+    sys.path.append('/Users/jamesmaskill/Desktop/Decision_Transformers/VS_Implementation/src')
+
+    from drawdown_control.decision_transformer import *
+    from drawdown_control.decision_transformer_policy import *
+    from drawdown_control.trajectory_processing import *
+    from drawdown_control.evaluate import *
+
+    import wandb
+    from UtilsRL.logger import CompositeLogger
+    from torch.utils.data import DataLoader
+    from UtilsRL.exp import parse_args, setup
+    import d4rl
+
+    """
+    The training regime for the Decision Transformers deviates from the original presentaiton of the model because we
+    are indifferent between different reward:[drawdown/episode length] pairs and concerned only with the model's ability 
+    to achieve the target reward-drawdown pair reliably. We therefore select both values randomly from the interval of possible 
+    values represented in the training data. Where multiple training datasets exists, these intervals are chosen as the 
+    maximum and minimum from the union of dataset trajectories. It is expected that loss curves from these targets will better
+    reflect the generalized control capability of the deicison transformer.
+    (The only exception to this methodology is the ignorance of the 'Expert' dataset (where available) in reflection of the 
+    fact that the maximum rewards and timesteps represented in their datasets are orders of magnitude greater than all the other
+    trajectories comprising an environment's datasets.)
+    """
+
+    def train_drawdown(env):
+        args = parse_args("config/walker/default_parameters.py")
+        exp_name = "_".join([args.task, "seed"+str(args.seed)]) 
+        logger = CompositeLogger(log_dir=f"./log/dt/{args.name}", name=exp_name, logger_config={
+            "TensorboardLogger": {}, 
+            "WandbLogger": {"config": args, "settings": wandb.Settings(_disable_stats=True), **args.wandb}
+        })
+        setup(args, logger)
+        env, dataset = get_d4rl_dataset(args.task, normalize_obs=args.normalize_obs, normalize_reward=args.normalize_reward, discard_last=False)
+        obs_shape = env.observation_space.shape[0]
+        action_shape = env.action_space.shape[-1]
+
+        offline_buffer = D4RLTrajectoryBuffer(dataset, seq_len=args.seq_len, return_scale=args.return_scale)
+        indices = [i for i, x in enumerate(dataset['terminals']) if x]
+        episode_lengths = [indices[0]] + [indices[i] - indices[i-1] for i in range(1, len(indices))]
+        max_len = 2048
+
+        dt = DecisionTransformer(
+            obs_dim=obs_shape, 
+            action_dim=action_shape, 
+            embed_dim=args.embed_dim, 
+            num_layers=args.num_layers, 
+            num_heads=args.num_heads, 
+            attention_dropout=args.attention_dropout, 
+            residual_dropout=args.residual_dropout, 
+            embed_dropout=args.embed_dropout, 
+            max_seq_len = max_len
+        ).to('cpu')
+
+        policy = DecisionTransformerPolicy(
+            dt=dt, 
+            state_dim=obs_shape,
+            action_dim=action_shape, 
+            embed_dim=args.embed_dim, 
+            seq_len=args.seq_len, 
+            episode_len=args.episode_len, 
+            use_abs_timestep=args.use_abs_timestep, 
+            policy_type=args.policy_type, 
+            device='cpu'
+        ).to('cpu')
+        policy.configure_optimizers(lr=args.lr, weight_decay=args.weight_decay, betas=args.betas, warmup_steps=args.warmup_steps)
+
+        # main loop   
+        policy.train()
+        trainloader = DataLoader(
+            offline_buffer, 
+            batch_size=args.batch_size, 
+            pin_memory=True,
+            num_workers=args.num_workers
+        )
+        trainloader_iter = iter(trainloader)
+        for i_epoch in range(1, args.max_epoch+1):
+            for i_step in range(args.step_per_epoch):
+                batch = next(trainloader_iter)
+                train_metrics = policy.update(batch, clip_grad=args.clip_grad)
+            
+            if i_epoch % 2 == 0:
+                eval_metrics = eval_decision_transformer(env, policy, rand(), args.return_scale, args.eval_episode, seed=args.seed, target_max_drawdown=rand())
+                logger.info(f"Episode {i_epoch}: \n{eval_metrics}")
+            
+            if i_epoch % args.log_interval == 0:
+                logger.log_scalars("", train_metrics, step=i_epoch)
+                logger.log_scalars("Eval", eval_metrics, step=i_epoch)
+                
+            if i_epoch % args.save_interval == 0:
+                logger.log_object(name=f"policy_{i_epoch}.pt", object=policy.state_dict(), path=f"./out/dt/d4rl/{args.name}/{args.task}/seed{args.seed}/policy/")
